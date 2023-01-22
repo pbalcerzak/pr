5ea7840500973645004f88459169f437f2828710
Piotr Balcerzak <piotr.balcerzak@bitbag.pl>
2023-01-22 04:47:12 +0100

    Final version IT WORKS!!!!

--- main.cpp
+++ main.cpp
@@ -5,15 +5,19 @@
 #include <vector>
 #include <cmath>
 #include <unistd.h>
-#include <time.h>
+#include <ctime>
 
 
 #define RED "\033[41m"
 #define GRN "\033[42m"
 #define YLW "\033[43m"
 #define NLC "\033[0m\n"
+#define CYA "\033[46m"
+
+#define PROPORTION 2
 
 using namespace std;
+using namespace std::chrono;
 
 enum Tag {
     REQ, ACK, ACCEPT
@@ -29,20 +33,28 @@ enum Type {
 };
 
 int energy = 10;
+int max_energy = 10;
 int max_rank;
 int my_rank;
 int my_time = 0;
 int yoda_count = 0;
 int zet_count = 0;
 int acknowledged = 0;
-Type my_type;
+int energyPossiblyTaken = 0;
+int energyPossiblyAdded = 0;
 pthread_mutex_t yodaSentMutex;
+pthread_mutex_t zetSentMutex;
 pthread_mutex_t timeMutex;
 pthread_mutex_t energyMutex;
 pthread_mutex_t acknowledgedMutex;
+pthread_mutex_t sendAckMutex;
+pthread_mutex_t energyPossiblyTakenMutex;
+pthread_mutex_t energyPossiblyAddedMutex;
 bool yodaSent = false;
+bool zetSent = false;
+std::vector<int> sendAck;
 
-#define pretty_print(format, ...) fprintf(stderr, "%6d P%d %s " format, my_time, my_rank, my_type == 0 ? "Yoda" : "Zet", ##__VA_ARGS__)
+#define pretty_print(format, ...) fprintf(stderr, "%6d P%d %s " format, my_time, my_rank, my_rank < yoda_count ? "Yoda" : "Zet", ##__VA_ARGS__)
 
 void tick(int new_time = 0) {
     pthread_mutex_lock(&timeMutex);
@@ -58,25 +70,38 @@ void send(int dst, Tag tag) {
 void *yodaCommunicationThread(void *arg) {
     Msg received_message{};
 
-//    pretty_print(GRN "startuje komunikacje" NLC);
     while (true) {
         MPI_Recv(&received_message, sizeof(Msg), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
                  MPI_STATUS_IGNORE);
 
         switch (received_message.tag) {
             case REQ:
-//                pretty_print(GRN "dostałem REQ od %d" NLC, received_message.rank);
-
                 pthread_mutex_lock(&yodaSentMutex);
+
                 if (!yodaSent) {
                     send(received_message.rank, ACK);
+                    tick(received_message.time);
+                    pthread_mutex_lock(&energyMutex);
+                    energy--;
+                    pthread_mutex_unlock(&energyMutex);
                 } else {
                     if (received_message.time > my_time ||
-                        (received_message.time == my_time && received_message.rank < my_rank)) {
-                        pretty_print(GRN "Dosta ACCEPT" NLC);
+                        (received_message.time == my_time && received_message.rank > my_rank)) {
                         pthread_mutex_lock(&acknowledgedMutex);
                         acknowledged += 1;
                         pthread_mutex_unlock(&acknowledgedMutex);
+
+                        pthread_mutex_lock(&sendAckMutex);
+                        sendAck.push_back(received_message.rank);
+                        pthread_mutex_unlock(&sendAckMutex);
+
+                        pthread_mutex_lock(&energyPossiblyTakenMutex);
+                        energyPossiblyTaken++;
+                        pthread_mutex_unlock(&energyPossiblyTakenMutex);
+                    } else {
+                        pthread_mutex_lock(&energyMutex);
+                        energy--;
+                        pthread_mutex_unlock(&energyMutex);
                     }
 
                 }
@@ -84,20 +109,72 @@ void *yodaCommunicationThread(void *arg) {
                 pthread_mutex_unlock(&yodaSentMutex);
                 break;
             case ACK:
-                tick(received_message.time);
-                pthread_mutex_lock(&energyMutex);
-                energy = min(energy, received_message.energy);
-                pthread_mutex_unlock(&energyMutex);
-//                pretty_print(RED "dostałem ACK od: P%d" NLC, received_message.rank);
+                pthread_mutex_lock(&acknowledgedMutex);
+                acknowledged += 1;
+                pthread_mutex_unlock(&acknowledgedMutex);
                 break;
             case ACCEPT:
-                pretty_print(GRN "dostałem ACCEPT" NLC);
+                pthread_mutex_lock(&energyMutex);
+                energy = max_energy;
+                pthread_mutex_unlock(&energyMutex);
                 break;
         }
     }
 }
 
-void *startComThreadZet(void *ptr);
+void *zetCommunicationThread(void *ptr) {
+    Msg received_message{};
+
+    while (true) {
+        MPI_Recv(&received_message, sizeof(Msg), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+
+        switch (received_message.tag) {
+            case REQ:
+                pthread_mutex_lock(&zetSentMutex);
+                if (!zetSent) {
+                    send(received_message.rank, ACK);
+                    tick(received_message.time);
+                    pthread_mutex_lock(&energyMutex);
+                    energy++;
+                    pthread_mutex_unlock(&energyMutex);
+                } else {
+                    if (received_message.time > my_time ||
+                        (received_message.time == my_time && received_message.rank > my_rank)) {
+                        pthread_mutex_lock(&acknowledgedMutex);
+                        acknowledged += 1;
+                        pthread_mutex_unlock(&acknowledgedMutex);
+
+                        pthread_mutex_lock(&sendAckMutex);
+                        sendAck.push_back(received_message.rank);
+                        pthread_mutex_unlock(&sendAckMutex);
+
+                        pthread_mutex_lock(&energyPossiblyAddedMutex);
+                        energyPossiblyAdded++;
+                        pthread_mutex_unlock(&energyPossiblyAddedMutex);
+                    } else {
+                        pthread_mutex_lock(&energyMutex);
+                        energy++;
+                        pthread_mutex_unlock(&energyMutex);
+                    }
+
+                }
+
+                pthread_mutex_unlock(&zetSentMutex);
+                break;
+            case ACK:
+                pthread_mutex_lock(&acknowledgedMutex);
+                acknowledged += 1;
+                pthread_mutex_unlock(&acknowledgedMutex);
+                break;
+            case ACCEPT:
+                pthread_mutex_lock(&energyMutex);
+                energy = 0;
+                pthread_mutex_unlock(&energyMutex);
+                break;
+        }
+    }
+}
 
 void yoda() {
     while (true) {
@@ -105,57 +182,131 @@ void yoda() {
             pthread_mutex_lock(&yodaSentMutex);
             if (!yodaSent) {
                 tick();
-                pretty_print(YLW "Ubiegam się o dostęp" NLC);
+                pretty_print(CYA "Ubiegam się o dostęp" NLC);
+
+                pthread_mutex_lock(&acknowledgedMutex);
+                acknowledged = 0;
+                pthread_mutex_unlock(&acknowledgedMutex);
+
                 for (int i = 0; i < yoda_count; i++) {
                     if (i != my_rank) {
                         send(i, REQ);
                     }
                 }
                 yodaSent = true;
-
-                pthread_mutex_lock(&acknowledgedMutex);
-                acknowledged = 0;
-                pthread_mutex_unlock(&acknowledgedMutex);
             }
             pthread_mutex_unlock(&yodaSentMutex);
 
-//            int energyPossiblyTaken = 0;
-//        pretty_print(RED "Moje acknowledged: %d" NLC, acknowledged);
-            if ((acknowledged == yoda_count - 1) && energy > 0 && yodaSent) {
-//            pretty_print(RED "Pobieram energie" NLC);
+            if ((acknowledged >= yoda_count - 1) && energy > 0 && yodaSent) {
                 pthread_mutex_lock(&energyMutex);
                 energy--;
-//                energy -= energyPossiblyTaken;
                 pretty_print(GRN "Pobieram energię zostało: %d" NLC, energy);
                 if (energy == 0) {
-                    pretty_print(RED "skończyła się energia!!!" NLC);
-                }
-                pthread_mutex_unlock(&energyMutex);
-                int sleepTime = rand() % (6 - 1) + 1;
-                tick();
-                for (int i = 0; i < yoda_count; i++) {
-                    if (i != my_rank) {
-//                    pretty_print(RED "wysyłam ACK do %d" NLC, i);
-                        send(i, ACK);
+                    pretty_print(RED "skończyła się energia, wysyłam request do Zetów" NLC);
+                    for (int i = yoda_count; i < max_rank; i++) {
+                        send(i, ACCEPT);
                     }
                 }
+                pthread_mutex_unlock(&energyMutex);
+
+                int sleepTime = rand() % (5 - 1) + 1;
+                tick();
+                for (auto &element: sendAck) {
+                    send(element, ACK);
+                }
+
+                pthread_mutex_lock(&energyMutex);
+                energy -= energyPossiblyTaken;
+                if (energy < 0) {
+                    energy = 0;
+                }
+                pthread_mutex_unlock(&energyMutex);
+
+                pthread_mutex_lock(&energyPossiblyTakenMutex);
+                energyPossiblyTaken = 0;
+                pthread_mutex_unlock(&energyPossiblyTakenMutex);
+
+                pthread_mutex_lock(&sendAckMutex);
+                sendAck.clear();
+                pthread_mutex_unlock(&sendAckMutex);
 
                 pthread_mutex_lock(&yodaSentMutex);
                 yodaSent = false;
                 pthread_mutex_unlock(&yodaSentMutex);
                 pretty_print(YLW "śpie przez: %d sekund" NLC, sleepTime);
                 sleep(sleepTime);
-                pretty_print(YLW "skończyłem spać" NLC);
             }
         } else {
-            pretty_print(RED "Czekam na uzupełnienie" NLC);
+//            pretty_print(RED "Czekam na uzupełnienie" NLC);
             sleep(1);
         }
     }
 }
 
 void zet() {
-    pretty_print(GRN "REQ time: %d" NLC, 12);
+    while (true) {
+        if (energy < max_energy) {
+            pthread_mutex_lock(&zetSentMutex);
+            if (!zetSent) {
+                tick();
+                pretty_print(CYA "Ubiegam się o dostęp" NLC);
+
+                pthread_mutex_lock(&acknowledgedMutex);
+                acknowledged = 0;
+                pthread_mutex_unlock(&acknowledgedMutex);
+
+                for (int i = yoda_count; i < max_rank; i++) {
+                    if (i != my_rank) {
+                        send(i, REQ);
+                    }
+                }
+                zetSent = true;
+            }
+            pthread_mutex_unlock(&zetSentMutex);
+
+            if ((acknowledged >= zet_count - 1) && energy < max_energy && zetSent) {
+                pthread_mutex_lock(&energyMutex);
+                energy++;
+                pretty_print(GRN "Uzupełniam energię jest: %d" NLC, energy);
+                if (energy == max_energy) {
+                    pretty_print(RED "Energia uzupełniona, wysyłam request do Yodów" NLC);
+                    for (int i = 0; i < yoda_count; i++) {
+                        send(i, ACCEPT);
+                    }
+                }
+                pthread_mutex_unlock(&energyMutex);
+
+                int sleepTime = rand() % (5 - 1) + 1;
+                tick();
+                for (auto &element: sendAck) {
+                    send(element, ACK);
+                }
+
+                pthread_mutex_lock(&energyMutex);
+                energy += energyPossiblyAdded;
+                if (energy > max_energy) {
+                    energy = max_energy;
+                }
+                pthread_mutex_unlock(&energyMutex);
+
+                pthread_mutex_lock(&energyPossiblyAddedMutex);
+                energyPossiblyAdded = 0;
+                pthread_mutex_unlock(&energyPossiblyAddedMutex);
+
+                pthread_mutex_lock(&sendAckMutex);
+                sendAck.clear();
+                pthread_mutex_unlock(&sendAckMutex);
+
+                pthread_mutex_lock(&zetSentMutex);
+                zetSent = false;
+                pthread_mutex_unlock(&zetSentMutex);
+                pretty_print(YLW "śpie przez: %d sekund" NLC, sleepTime);
+                sleep(sleepTime);
+            }
+        } else {
+            sleep(1);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -164,11 +315,9 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &max_rank);
 
-//    my_type = Type(my_rank%2);
-//    yoda_count = (int) ceil(max_rank/2.0);
-//    zet_count = max_rank - yoda_count;
-    my_type = Type(0);
-    yoda_count = max_rank;
+    yoda_count = max_rank/PROPORTION;
+    zet_count = max_rank - yoda_count;
+
     srand(my_rank * time(nullptr));
 
     pthread_t threads;
@@ -176,17 +325,18 @@ int main(int argc, char **argv) {
     timeMutex = PTHREAD_MUTEX_INITIALIZER;
     energyMutex = PTHREAD_MUTEX_INITIALIZER;
     acknowledgedMutex = PTHREAD_MUTEX_INITIALIZER;
+    sendAckMutex = PTHREAD_MUTEX_INITIALIZER;
 
-    switch (my_type) {
-        case Y:
-            yodaSentMutex = PTHREAD_MUTEX_INITIALIZER;
-            pthread_create(&threads, nullptr, yodaCommunicationThread, nullptr);
-            yoda();
-            break;
-//        case Z:
-//            zet();
-//            pthread_create(&threads, nullptr, startComThreadZet, nullptr);
-//            break;
+    if (my_rank < yoda_count) {
+        yodaSentMutex = PTHREAD_MUTEX_INITIALIZER;
+        energyPossiblyTakenMutex = PTHREAD_MUTEX_INITIALIZER;
+        pthread_create(&threads, nullptr, yodaCommunicationThread, nullptr);
+        yoda();
+    } else {
+        zetSentMutex = PTHREAD_MUTEX_INITIALIZER;
+        energyPossiblyAddedMutex = PTHREAD_MUTEX_INITIALIZER;
+        pthread_create(&threads, nullptr, zetCommunicationThread, nullptr);
+        zet();
     }
 
     MPI_Finalize();
